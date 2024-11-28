e24c7f714f037a68daca6455aefe3e55b419d6cc
rnmensa <mensah.randy447@gmail.com>
2024-11-28 17:47:04 +0000

    Final look of App
    Beautiful and intuitive

--- static/js/script.js
+++ static/js/script.js
@@ -7,21 +7,3 @@ function displayFileName() {
     const fileName = fileInput.files.length > 0 ? fileInput.files[0].name : "No file chosen";
     document.getElementById('file-name').textContent = fileName;
 }
-
-// Function to handle the form validation
-document.getElementById('file-form').addEventListener('submit', function(event) {
-    var fileInput = document.getElementById('file-upload');
-    var errorMessage = document.getElementById('error-message');
-
-    // Check if no file is selected
-    if (!fileInput.files.length) {
-        // Prevent form submission
-        event.preventDefault();
-
-        // Display error message
-        errorMessage.style.display = 'block';
-    } else {
-        // Hide error message if a file is selected
-        errorMessage.style.display = 'none';
-    }
-});
